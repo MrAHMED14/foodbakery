b2b2864cad5cc7edbc9510cf4631883f41dd7dcf
MrAHMED <chikhaoui084@gmail.com>
2025-04-06 07:23:57 +0200

    feat: add 403 Forbidden error page with custom styling

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>403 Forbidden</title>
+    <style>
+        @import url("https://fonts.googleapis.com/css?family=Share+Tech+Mono|Montserrat:700");
+
+        * {
+            margin: 0;
+            padding: 0;
+            border: 0;
+            font-size: 100%;
+            font: inherit;
+            vertical-align: baseline;
+            box-sizing: border-box;
+            color: inherit;
+        }
+
+        body {
+            background-image: linear-gradient(120deg, #161616 0%, #000000 100%);
+            height: 100vh;
+        }
+
+        h1 {
+            font-size: 20vw;
+            text-align: center;
+            position: fixed;
+            width: 100vw;
+            z-index: 1;
+            color: #ffffff25;
+            top: 50%;
+            transform: translateY(-50%);
+            font-family: "Montserrat", monospace;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>403</h1>
+</body>
+
+</html>
