361ca7a39f70073a9b9c8f6520ef1f245eebd7b2
MrAHMED <chikhaoui084@gmail.com>
2025-04-08 01:29:55 +0200

    refactor: update restaurant views by including header component and removing redundant code

--- resources/views/front/restaurant/memberships.blade.php
+++ resources/views/front/restaurant/memberships.blade.php
@@ -7,37 +7,8 @@
 @section('content')
 <!-- Main Section Start -->
 <div class="main-section">
-    <div class="page-section restaurant-detail-image-section" style=" background: url({{asset('front/extra-images/cover-photo01.jpg')}}) no-repeat scroll 0 0 / cover;">
-        <!-- Container Start -->
-        <div class="container">
-            <!-- Row Start -->
-            <div class="row">
-                <!-- Column Start -->
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <div class="company-info-detail">
-                        <div class="company-info">
-                            <div class="img-holder">
-                                <figure>
-                                    <img src="{{asset('front/extra-images/listing-logo18.png')}}" alt="">
-                                </figure>
-                            </div>
+    @include('front.restaurant.body.header')
 
-                            <div class="text-holder">
-                                <span class="restaurant-title">Restaurant Demo</span>
-                                <div class="text">
-                                    <i class="icon-local_pizza"></i>
-                                    <p>Apple Juice</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Column End -->
-            </div>
-            <!-- Row End -->
-        </div>
-        <!-- Container End -->
-    </div>
     <div class="page-section account-header buyer-logged-in">
         <div class="container">
             <div class="row">
