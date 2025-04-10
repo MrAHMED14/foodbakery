45342676e3ce13d5e0d7e06bdb0c428ca80d3c6f
MrAHMED <chikhaoui084@gmail.com>
2025-04-10 07:21:46 +0200

    feat: add route to remove a dish from the cart

--- routes/web.php
+++ routes/web.php
@@ -77,6 +77,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
         Route::post('/cart/add', [CartController::class, 'add'])->name('cart.add');
         Route::get('/cart', [CartController::class, 'view'])->name('cart.view');
         Route::post('/cart/clear', [CartController::class, 'clear'])->name('cart.clear');
+        Route::post('/cart/remove', [CartController::class, 'removeDish'])->name('cart.removeDish');
         Route::post('/checkout', [OrderController::class, 'checkout'])->name('checkout');
 
         Route::group(['prefix' => 'buyer', 'as' => 'buyer.'], function () {
