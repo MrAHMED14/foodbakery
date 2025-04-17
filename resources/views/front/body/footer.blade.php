1273caf1b0fe83af7d0c8dd5e8111abc76e42a78
MrAHMED <chikhaoui084@gmail.com>
2025-04-17 10:42:53 +0200

    feat: update footer layout with new styles and content

--- resources/views/front/body/footer.blade.php
+++ resources/views/front/body/footer.blade.php
@@ -1,114 +1,114 @@
-<!-- Footer Start -->
-<footer id="footer" class="footer-style-2">
-    <div class="footer-widget">
+
+
+<footer id="footer" class="footer-style-1">
+    <div class="footer-widget" style="background-color: #121418 !important;">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <div class="widget widget-newsletter">
-                        <div class="widget-title">
-                            <h5>Subscribe to our Newsletter</h5>
-                        </div>
-                        <div class="fieldset">
-                            <form action="#">
-                                <div class="field-holder">
-                                    <label>
-                                        <i class=" icon-envelope3"></i>
-                                        <input type="text" class="field-input" placeholder=" Enter Your Email Address...">
-                                    </label>
+                    <div class="row">
+                        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                            <div class="widget widget-top-cities">
+                                <div class="widget-title">
+                                    <h5 style="color: #ffffff !important;">Popular Cities</h5>
                                 </div>
-                                <div class="field-holder btn-holder">
-                                    <input class="subscribe-btn bgcolor" type="submit" value="Sign Up">
+                                <ul>
+                                    <li><a href="{{route('front.result')}}">Tiaret</a></li>
+                                    <li><a href="{{route('front.result')}}">Sougueur</a></li>
+                                    <li><a href="{{route('front.result')}}">Mahdia</a></li>
+                                    <li><a href="{{route('front.result')}}">Chalala</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                            <div class="widget widget-top-cities">
+                                <div class="widget-title">
+                                    <h5 style="color: #ffffff !important;">Popular Cuisnies</h5>
                                 </div>
-                            </form>
+                                <ul>
+                                    <li><a href="#">Apple Juice</a></li>
+                                    <li><a href="#">BB.Q</a></li>
+                                    <li><a href="#">Chicken Roast</a></li>
+                                    <li><a href="#">Hot Dogs</a></li>
+                                    <li><a href="#">Prawns</a></li>
+                                    <li><a href="#">Steam Roast</a></li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                    <div class="widget widget_text">
-                        <div class="widget-title">
-                            <h5>About us</h5>
+                        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                            <div class="widget widget-categories">
+                                <div class="widget-title">
+                                    <h5 style="color: #ffffff !important;">Menu</h5>
+                                </div>
+                                <ul>
+                                    <li><a href="#"> Home</a></li>
+                                    <li><a href="#"> Blog Large</a></li>
+                                    <li><a href="#"> Contact</a></li>
+                                    <li><a href="#"> FAQ’s</a></li>
+                                    <li><a href="#"> How it works</a></li>
+                                </ul>
+                            </div>
                         </div>
-                        <div class="textwidget">Foodbakery is the business of food and restaurants.
-                            Ridiculus sociosqu cursus neque cursus curae ante scelerisque vehicula.
-                            Explore restaurants, bars, and cafés by locality cum doctus civibus
-                            efficiantur in imperdiet deterruisset. FoodBakery Just ordered me some grub
+                        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                            <div class="widget widget-connect">
+                                <div class="widget-title">
+                                    <h5 style="color: #ffffff !important;"> Connect</h5>
+                                </div>
+                                <ul class="social-media">
+                                    <li><a href="#" data-original-title="twitter" style="background-color: #ffffff; color: #121418;"><i class=" icon-twitter"></i></a>
+                                    </li>
+                                    <li><a href="#" data-original-title="linkedin" style="background-color: #ffffff; color: #121418;"><i class="icon-linkedin"></i></a>
+                                    </li>
+                                    <li><a href="#" data-original-title="pinterest" style="background-color: #ffffff; color: #121418;"><i class="icon-pinterest"></i></a>
+                                    </li>
+                                    <li><a href="#" data-original-title="google" style="background-color: #ffffff; color: #121418;"><i class="icon-google"></i></a>
+                                    </li>
+                                    <li><a href="#" data-original-title="instagram" style="background-color: #ffffff; color: #121418;"><i class="icon-instagram"></i></a>
+                                    </li>
+                                    <li><a href="#" data-original-title="youtube" style="background-color: #ffffff; color: #121418;"><i class="icon-youtube"></i></a>
+                                    </li>
+                                </ul>
+
+                                <ul>
+                                    <li><span class="bgcolor" style="color: #ffffff !important; height: 36px;"><i class="icon-ring_volume" style="color: #ffffff !important;"></i></span>
+                                        <p>+1 (321) 828 6662</p>
+                                    </li>
+                                    <li><span class="bgcolor" style="color: #ffffff !important; height: 36px;"><i class="icon-envelope-o" style="color: #ffffff !important;"></i></span>
+                                        <p><a href="#">support@chimpstudio.com</a></p>
+                                    </li>
+                                    <li><span class="bgcolor" style="color: #ffffff !important; height: 36px;"><i class="icon-location-pin2" style="color: #ffffff !important;"></i></span>
+                                        <p><a href="#">33 - B, 2nd Floor, Saint Jhon Doe Appartments, Sussex, UK.26AL565C.</a></p>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12">
-                    <div class="widget widget-top-cities">
-                        <div class="widget-title">
-                            <h5>Popular Cities</h5>
-                        </div>
-                        <ul>
-                            <li><a href="{{route('front.result')}}">Tiaret</a></li>
-                            <li><a href="{{route('front.result')}}">Sougueur</a></li>
-                            <li><a href="{{route('front.result')}}">Mahdia</a></li>
-                            <li><a href="{{route('front.result')}}">Chalala</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12">
-                    <div class="widget widget-top-cities">
-                        <div class="widget-title">
-                            <h5>Popular Cuisines</h5>
-                        </div>
-                        <ul>
-                            <li><a href="{{route('front.listings')}}">BB.Q</a></li>
-                            <li><a href="{{route('front.listings')}}">ChickenRoast</a></li>
-                            <li><a href="{{route('front.listings')}}">Cold Coffee</a></li>
-                            <li><a href="{{route('front.listings')}}">Cold Drink</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12">
-                    <div class="widget widget-categories">
-                        <div class="widget-title">
-                            <h5>Menu</h5>
-                        </div>
-                        <ul>
-                            <li><a href="{{route('front.contact')}}">Contact</a></li>
-                            <li><a href="{{route('front.price')}}">Price Plans</a></li>
-                            <li><a href="{{route('front.how')}}">How itworks</a></li>
-                            <li><a href="{{route('front.faq')}}">FAQ’s</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                    <div class="widget widget-connect">
-                        <div class="widget-title">
-                            <h5> Connect</h5>
-                        </div>
-                        <ul>
-                            <li><span class="bgcolor"><i class="icon-ring_volume"></i></span>
-                                <p>+1 123 456 7892</p>
-                            </li>
-                            <li><span class="bgcolor"><i class="icon-envelope-o"></i></span>
-                                <p><a href="#">support@chimpstudio.com</a>
-                                </p>
-                            </li>
-                            <li><span class="bgcolor"><i class="icon-location-pin2"></i></span>
-                                <p>33 - B, 2nd Floor, Saint Jhon Doe Appartments, Sussex, UK.26AL565C.</p>
-                            </li>
-                        </ul>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="copyright-sec">
+    <div class="copyright-sec" style="background-color: #121418 !important;">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="copyright-inner">
-                        <div class="copy-right">
-                            <p>© 2020 Foodbakery. All Rights Reserved. Powered By <a href="#">ChimpGroup</a>.
+                        <div class="copy-right" style="background-color: #121418 !important;">
+                            <p>
+                                © 2025 Foodbakery. All Rights Reserved.
+                            </p>
+                        </div>
+                        <div class="right-logos">
+                            <p>
+                                <img src="{{ asset('front/extra-images/footer-logo1.png') }}" alt="">
+                                <img src="{{ asset('front/extra-images/footer-logo2.png') }}" alt="">
+                                <img src="{{ asset('front/extra-images/footer-logo3.png') }}" alt="">
+                                <img src="{{ asset('front/extra-images/footer-logo4.png') }}" alt="">
+                                <img src="{{ asset('front/extra-images/footer-logo5.png') }}" alt="">
+                                <img src="{{ asset('front/extra-images/footer-logo6.png') }}" alt="">
                             </p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div> <!-- /.cs-copyright -->
+    </div>
 </footer>
-<!-- Footer End -->
