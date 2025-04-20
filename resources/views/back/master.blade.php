7a7584c1fc73e46a87c46b4ef39d567247506217
MrAHMED <chikhaoui084@gmail.com>
2025-04-20 21:18:52 +0200

    feat: change favicon and metadata for the site

--- resources/views/back/master.blade.php
+++ resources/views/back/master.blade.php
@@ -6,7 +6,12 @@
     <title> @yield('title', 'FoodBakery | Dashboard')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- App favicon -->
-    <link rel="shortcut icon" href="{{ asset('back/images/favicon.ico') }}">
+    <link rel="icon" type="image/png" href="{{ asset('/favicon-96x96.png') }}" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="{{ asset('/favicon.svg') }}" />
+    <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}" />
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/apple-touch-icon.png') }}" />
+    <meta name="apple-mobile-web-app-title" content="Food Bakery" />
+    <link rel="manifest" href="{{ asset('/site.webmanifest') }}" />
     <!-- plugin css -->
     <link href="{{ asset('back/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css') }}" rel="stylesheet"
         type="text/css" />
